c7425603ed4dc7890b920bb6a2975f6d1ace2e54
Kamil Czenczek <praca.hoorder@o2.pl>
2025-03-06 01:20:16 +0100

    feat(teams) add style and logic

--- app/api/leader/employee/route.js
+++ app/api/leader/employee/route.js
@@ -60,59 +60,6 @@ export async function PUT(req) {
     }
 }
 
-// export async function POST(req) {
-//     try {
-//         const body = await req.json();
-//         const {
-//             managerId,
-//             employeeId,
-//             transportFrom,
-//             transportTo,
-//             orderingPhoneNumber,
-//         } = body;
-
-//         const query = `INSERT INTO transport_orders
-//             (manager_id_fk,
-//             employee_id_fk,
-//             transport_from,
-//             transport_to,
-//             contact_number,
-//             order_created_at)
-//             VALUES (?,?,?,?,?,CURRENT_TIMESTAMP())`;
-
-//         const [result] = await db.query(query, [
-//             managerId,
-//             employeeId,
-//             transportFrom,
-//             transportTo,
-//             orderingPhoneNumber,
-//         ]);
-
-//         if (result.affectedRows === 1) {
-//             const [idResult] = await db.query(
-//                 "SELECT LAST_INSERT_ID() as id_transport_orders"
-//             );
-//             const id_transport_order = idResult[0].id_transport_orders;
-
-//             return jsonResponse({
-//                 message: "Dane zostały zapisane pomyślnie!",
-//                 id_transport_order,
-//             });
-//         } else {
-//             return jsonResponse(
-//                 { error: "Nie udało się zapisać danych." },
-//                 500
-//             );
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         return jsonResponse(
-//             { error: "Wystąpił błąd podczas dodawania zadania." },
-//             500
-//         );
-//     }
-// }
-
 export async function DELETE(req) {
     try {
         const { employeeId } = await req.json();
